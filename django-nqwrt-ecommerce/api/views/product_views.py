85a8085bc53a15cb230bdc9bac043ce78c7089b5
heo <admin@admin.com>
2025-05-06 00:49:54 +0900

    commit

--- django-nqwrt-ecommerce/api/views/product_views.py
+++ django-nqwrt-ecommerce/api/views/product_views.py
@@ -9,7 +9,22 @@ from api.serializers.product_serializers import ProductSerializer
 
 # Create your views here.
 
-
+# dev_10_3_Fruit
+from drf_spectacular.utils import extend_schema,OpenApiParameter
+# 함수형 뷰
+@extend_schema(
+    methods=['GET'], # 이 데코레이터를 적용할 HTTP 메서드
+    summary='상품들을 조회', # API 요약 설명
+    description='상품의 리스트를 조회하는 API입니다.', # API 상세 설명
+    responses={200: ProductSerializer(many=True)}, # 응답 스키마
+)
+@extend_schema(
+    methods=['POST'], # 이 데코레이터를 적용할 HTTP 메서드
+    summary='상품을 생성', # API 요약 설명
+    description='상품을 생성하는 API입니다.', # API 상세 설명
+    request=ProductSerializer,
+    responses={200: ProductSerializer(many=False)}, # 응답 스키마
+)
 # dev_29 추가 되도록
 @api_view(["GET", "POST"])
 def products_api(request):

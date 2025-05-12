fb65ee87fab2b6e61d28329510143cf629d238a3
heo <admin@admin.com>
2025-05-12 10:19:35 +0900

    commit

--- react-vite-hjs-ecommerce/src/ui/components/Navbar.jsx
+++ react-vite-hjs-ecommerce/src/ui/components/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
 
   //dev_5_Fruit
   const {user,logout} = useAuth();
+
+  
   console.log("유저")
   console.log(user)
 
   const logoutHandle = () => {
     logout()
+
   };
 
   //dev_6_Fruit 장바구니 카트 숫자

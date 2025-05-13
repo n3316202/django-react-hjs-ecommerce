5bef152ad3915d3bf030768f7cc71a8223c31ef3
heo <admin@admin.com>
2025-05-13 16:25:21 +0900

    commit

--- react-vite-hjs-ecommerce/src/ui/components/login/Login.jsx
+++ react-vite-hjs-ecommerce/src/ui/components/login/Login.jsx
@@ -40,7 +40,7 @@ const handleKakaoLogin = () => {
 
 
   window.Kakao.Auth.login({
-    scope: 'profile_nickname, account_email, gender', // 원하는 scope
+    scope: 'profile_nickname, account_email', // 원하는 scope
     success: async function (authObj) {
       const kakaoAccessToken = authObj.access_token
       console.log('Kakao Access Token:', kakaoAccessToken)
